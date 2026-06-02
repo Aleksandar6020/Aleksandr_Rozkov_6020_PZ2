f32c46ba9fa29fc56438e68bd59fa11e07e6bf00
Alexandr <rozhkov.a.ju@gmail.com>
2026-06-02 18:00:09 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/aleksandr_rozkov_6020_pz2/AleksandrRozkov6020Pz2Application.java
@@ -0,0 +1,13 @@
+package com.example.aleksandr_rozkov_6020_pz2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleksandrRozkov6020Pz2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AleksandrRozkov6020Pz2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aleksandr_rozkov_6020_pz2/AleksandrRozkov6020Pz2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aleksandr_rozkov_6020_pz2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleksandrRozkov6020Pz2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
